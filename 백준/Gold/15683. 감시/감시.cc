da949fc86952f2d73f700cfe37953c574bcbc37c
donghyun kim <donghyun1998@naver.com>
2023-09-18 20:32:13 +0900

    [Gold IV] Title: 감시, Time: 44 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -24,71 +24,45 @@ int countZero() {
 	return cnt;
 }
 
+void changeMonitoringArea(int x, int y, int curDx, int curDy) {
+	while (x < col && x >= 0 && y < row && y >= 0 && board[x][y] != 6) {
+		if (board[x][y] == 0)
+			board[x][y] = 7; // 감시영역 7로 표시
+		x += curDx;
+		y += curDy;
+	}
+}
+
 void runCctv(int cctvIdx, int direction) {
-	int beginX = cctv[cctvIdx].first;
-	int beginY = cctv[cctvIdx].second;
-	int cctvType = board[beginX][beginY];
+	int x = cctv[cctvIdx].first;
+	int y = cctv[cctvIdx].second;
+	int cctvType = board[x][y];
 	int curDx, curDy;
 
-	if (true) {
-		// 오른쪽
-		int x = beginX, y = beginY;
+	if (true) {// 오른쪽
 		curDx = dx[(0 + direction) % 4];
 		curDy = dy[(0 + direction) % 4];
-		while (x < col && x >= 0 && y < row && y >= 0 && board[x][y] != 6) {
-			if (board[x][y] == 0)
-				board[x][y] = 7; // 감시영역 7로 표시
-			x += curDx;
-			y += curDy;
-		}
+		changeMonitoringArea(x, y, curDx, curDy);
 	}
-	if (cctvType == 2 || cctvType == 4 || cctvType == 5) {
-		// 왼쪽
-		int x = beginX, y = beginY;
+	if (cctvType == 2 || cctvType == 4 || cctvType == 5) {// 왼쪽
 		curDx = dx[(2 + direction) % 4];
 		curDy = dy[(2 + direction) % 4];
-		while (x < col && x >= 0 && y < row && y >= 0 && board[x][y] != 6) {
-			if (board[x][y] == 0)
-				board[x][y] = 7; // 감시영역 7로 표시
-			x += curDx;
-			y += curDy;
-		}
+		changeMonitoringArea(x, y, curDx, curDy);
 	}
-	if (cctvType == 3 || cctvType == 4 || cctvType == 5) {
-		// 위
-		int x = beginX, y = beginY;
+	if (cctvType == 3 || cctvType == 4 || cctvType == 5) {// 위
 		curDx = dx[(3 + direction) % 4];
 		curDy = dy[(3 + direction) % 4];
-		while (x < col && x >= 0 && y < row && y >= 0 && board[x][y] != 6) {
-			if (board[x][y] == 0)
-				board[x][y] = 7; // 감시영역 7로 표시
-			x += curDx;
-			y += curDy;
-		}
+		changeMonitoringArea(x, y, curDx, curDy);
 	}
-	if (cctvType == 5) {
-		// 아래
-		int x = beginX, y = beginY;
+	if (cctvType == 5) {// 아래
 		curDx = dx[(1 + direction) % 4];
 		curDy = dy[(1 + direction) % 4];
-		while (x < col && x >= 0 && y < row && y >= 0 && board[x][y] != 6) {
-			if (board[x][y] == 0)
-				board[x][y] = 7; // 감시영역 7로 표시
-			x += curDx;
-			y += curDy;
-		}
+		changeMonitoringArea(x, y, curDx, curDy);
 	}
 }
 
 void dfs(int depth) { // cctv 돌면서 4방향 보고있을 때 다 생각할거임
 	if (depth == cctv.size()) { // cctv 다 봤음
-		// for (int i = 0; i < col; i++) {
-		// 	for (int j = 0; j < row; j++) {
-		// 		cout << board[i][j] << ' ';
-		// 	}
-		// 	cout << endl;
-		// }
-		// cout << endl;
 		res = min(countZero(), res);
 		return ;
 	}
