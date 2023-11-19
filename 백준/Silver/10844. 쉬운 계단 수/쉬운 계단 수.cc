12a51bf122fe4021ff6cd7fc5c3bd8419c7c6f5a
donghyun kim <donghyun1998@naver.com>
2023-11-20 01:08:25 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <unordered_set>
+#include <cstring>
+
+using namespace	std;
+long long dp[101][10];
+
+int	main()
+{
+	ios::sync_with_stdio(0),cin.tie(0);
+	int N;
+	cin >> N;
+
+	for (int i = 1; i < 10; i++)
+		dp[1][i] = 1;
+
+	for (int i = 2; i <= N; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (j == 0)
+				dp[i][0] = dp[i - 1][j + 1];
+			else if (j == 9)
+				dp[i][9] = dp[i - 1][j - 1];
+			else
+				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+
+			dp[i][j] %= 1000000000;
+		}
+	}
+	long long res = 0;
+	for (int i = 0; i < 10; i++) {
+		res += dp[N][i];
+		res %= 1000000000;
+	}
+	cout << res;
+}
